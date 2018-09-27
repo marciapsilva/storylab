694c0431314b2691862b281f4c36881615136b5a
msilva <marcia.silva1802@gmail.com>
2018-09-27 02:53:32 -0300

    html do index e planejamento do js

--- dist/app.js
+++ dist/app.js
@@ -9,3 +9,33 @@ function renderLogin() {
 function renderProjects() {
   return '<p>Aqui estão os PROJETO tudo AÍ CARAIO.</p>';
 }
+
+//função do HOME
+    //página de boas-vindas e CADASTRO
+
+//função do botão ENTRAR (com firebase)
+    //mudar o navegador com o usuário logado (trocar os inputs de logar para CONFIGURACOES e SAIR)
+    //salvar no local storage
+
+  //PERGUNTAR PRO RAFA SE NO ROTAS VAO SÓ AS ROTAS OU PODE TER OUTRAS COISAS
+
+//função de cada PROJETO para a página de funcionalidades
+    //criar funcionalidade de ADICIONAR projeto (modal + banco de dados)
+    //criar funcionalidade de EDITAR projeto (modal + banco de dados)
+    //criar funcionalidade de DELETAR projeto (modal + banco de dados)
+
+//função da funcionalidade de REFERENCIA
+    //criar funcionalidade de ADICIONAR referencia de LIVRO (modal + banco de dados)
+    //criar funcionalidade de ADICIONAR referencia de FILME (modal + banco de dados)
+
+    //criar funcionalidade de EDITAR referencia de LIVRO (modal + banco de dados)
+    //criar funcionalidade de EDITAR referencia de FILME (modal + banco de dados)
+
+    //criar funcionalidade de DELETAR referencia de LIVRO (modal + banco de dados)
+    //criar funcionalidade de DELETAR referencia de FILME (modal + banco de dados)
+
+    //implementar a API de livros
+    //implementar a API de filmes
+
+//função do micro menu do projeto (RETORNAR para a página com as funcionalidades e todos os projetos)
+
--- dist/routes.js
+++ dist/routes.js
@@ -14,3 +14,15 @@ function login() {
 function projects() {
   $('main').html(renderProjects());
 }
+
+//rota do HOME
+
+//rota do botão ENTRAR (com firebase)
+
+  //PERGUNTAR PRO RAFA SE NO ROTAS VAO SÓ AS ROTAS OU PODE TER OUTRAS COISAS
+
+//rota de cada PROJETO para a página de funcionalidades
+
+//rota da funcionalidade de REFERENCIA
+
+//rotas do micro menu do projeto (RETORNAR para a página com as funcionalidades e todos os projetos)
